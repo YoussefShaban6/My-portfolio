1c59a6b060f9927f76b40282b68834a05beae5b7
Youssef1S <yosifshaban6@gmail.com>
2022-07-18 13:32:22 +0200

    Solve the problems

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ import myWorks from "./Projects.js";
 
 const navLabel = document.querySelectorAll(".label");
 const worksContainer = document.querySelector(".work-grid");
-
 // Nav Bar
 const labels = [
   { name: "Hello,", link: "About" },
@@ -27,10 +26,32 @@ worksContainer.innerHTML = "";
 myWorks.forEach((work, index) => {
   worksContainer.innerHTML += `
   <div class="work">
+  <div class="work--img-container">
   <img src="${myWorks[index].src}" alt="Porject Image" class="work-img">
-  <div class="preview">
-   <a href="${myWorks[index].srcCode}" class="preview-link"><img src="imgs/logos/github.png" alt="" class="preview--img"></a>
-   <a href="${myWorks[index].live}" class="preview-link"><img src="imgs/logos/view.png" alt="" class="preview--img"></a>
+  </div>
+  <div class="preview hidden">
+  <a href="${myWorks[index].srcCode}" class="preview-link"><img src="imgs/logos/github.png" alt="" class="preview--img"></a>
+  <a href="${myWorks[index].live}" class="preview-link"><img src="imgs/logos/view.png" alt="" class="preview--img"></a>
   </div>
   </div>`;
 });
+
+const worksImg = document.querySelectorAll(".work-img");
+const works = document.querySelectorAll(".work");
+const previews = document.querySelectorAll(".preview");
+
+// console.log(previews)
+
+works.forEach((work, index) => {
+  work.addEventListener("mouseover", () => {
+    worksImg[index].style.opacity = "0.5";
+    previews[index].classList.remove("hidden");
+  });
+  work.addEventListener("mouseout", () => {
+    worksImg[index].style.opacity = "1";
+    
+    previews[index].classList.add("hidden");
+  });
+});
+
+
