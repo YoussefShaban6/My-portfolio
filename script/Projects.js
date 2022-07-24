ba271c462e8521aa5ef02e49e2d54d716d424eb4
Youssef1S <yosifshaban6@gmail.com>
2022-07-24 19:17:10 +0200

    Modify animation in works' section

--- script/Projects.js
+++ script/Projects.js
@@ -1,41 +1,48 @@
 const myWorks = [
   {
+    name: "Omnifood Project",
     src: "./imgs/projects/omnifood.png",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/11-HTML%26CSS/07-Omnifood-project",
     live: "https://omnifood101.netlify.app/",
   },
   {
+    name: "Pig Game",
     src: "./imgs/projects/pig-game.png",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/12-JavaScript/05-DOM-and-Events-fundamentals/03-Pig-Game",
     live: "https://piggame101.netlify.app/",
   },
   {
+    name: "Bankist App",
     src: "./imgs/projects/bankist.png",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/12-JavaScript/09-Working-with-Arrays/Bankist-app",
     live: "https://bankistapp101.netlify.app/",
   },
   {
+    name: "Bankist Website",
     src: "./imgs/projects/bankist-website.png",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/12-JavaScript/11-Advanced-DOM-and-Events/Bankist-website",
     live: "https://bankist-website101.netlify.app/",
   },
   {
+    name: "Mapty App",
     src: "./imgs/projects/mapty.png",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/12-JavaScript/13-Mapty-App",
     live: "https://maptyapp101.netlify.app/",
   },
   {
+    name: "Expenses Manager",
     src: "./imgs/projects/Expenses-manager.png",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/13-React-Js/03-Rendering-Lists-of-Data",
     live: "https://expenses-manager101.netlify.app/",
   },
   {
+    name: "Goal App",
     src: "./imgs/projects/goal-app.jpg",
     srcCode:
       "https://github.com/Youssef1S/Kalbonyan_Elmarsos/tree/main/Udemy/13-React-Js/04-React-styling",
--- script/script.js
+++ script/script.js
@@ -28,10 +28,13 @@ myWorks.forEach((work, index) => {
   <div class="work">
   <div class="work--img-container">
   <img src="${myWorks[index].src}" alt="Porject Image" class="work-img">
-  </div>
   <div class="preview hidden">
-  <a href="${myWorks[index].srcCode}" class="preview-link"><img src="imgs/logos/github.png" alt="" class="preview--img"></a>
-  <a href="${myWorks[index].live}" class="preview-link"><img src="imgs/logos/view.png" alt="" class="preview--img"></a>
+  <p class="preview--txt">${myWorks[index].name}</p>
+  <div class="preview--view">
+  <a href="${myWorks[index].srcCode}" class="preview-link"><img src="imgs/logos/github-brands.svg" alt="" class="preview--img"></a>
+  <a href="${myWorks[index].live}" class="preview-link"><img src="imgs/logos/eye-solid.svg" alt="" class="preview--img"></a>
+  </div>
+  </div>
   </div>
   </div>`;
 });
@@ -44,14 +47,13 @@ const previews = document.querySelectorAll(".preview");
 
 works.forEach((work, index) => {
   work.addEventListener("mouseover", () => {
-    worksImg[index].style.opacity = "0.5";
+    worksImg[index].classList.add("work-img--scale");
     previews[index].classList.remove("hidden");
+    // previews[index].classList.add("animate__zoomIn");
   });
   work.addEventListener("mouseout", () => {
-    worksImg[index].style.opacity = "1";
-    
+    // previews[index].classList.remove("animate__zoomIn");
     previews[index].classList.add("hidden");
+    worksImg[index].classList.remove("work-img--scale");
   });
 });
-
-
